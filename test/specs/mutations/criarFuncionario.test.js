c31b1d81f6c52e09447d1e60a637707ddcd33099
carlosrcarletto <carlosrcarletto@gmail.com>
2026-09-14 19:21:56 -0300

    atulizacao final

--- test/specs/mutations/criarFuncionario.test.js
+++ test/specs/mutations/criarFuncionario.test.js
@@ -1,17 +1,18 @@
 const request = require("supertest");
 const { expect } = require("chai");
 
-describe("Mutation - Criar Funcionario", () => {
+describe("Mutation - Criar Funcionário", () => {
   let token;
+
   before(async () => {
     const resposta = await request("http://localhost:4000")
       .post("/graphql")
       .send({
-        query: `mutation Mutation($email: String!, $senha: String!) {
-        login(email: $email, senha: $senha) {
-              token 
-        }
-      }`,
+        query: `mutation Login($email: String!, $senha: String!) {
+                    login(email: $email, senha: $senha) {
+                        token
+                    }
+                }`,
         variables: {
           email: "admin@admin.com",
           senha: "123456",
@@ -22,35 +23,198 @@ describe("Mutation - Criar Funcionario", () => {
     expect(resposta.body.data.login).to.have.property("token");
     token = resposta.body.data.login.token;
   });
-  it.only("deve criar um funcionario quando preencho os campos obrigatorios de forma valida", async () => {
+
+  it("deve criar um funcinário quando preencho os campos obrigatórios de forma válida", async () => {
     let cpf = Date.now();
-    console.log(cpf);
 
     const resposta = await request("http://localhost:4000")
       .post("/graphql")
       .set("Authorization", `Bearer ${token}`)
       .send({
-        query: `mutation Mutation($input: CriarFuncionarioInput!) {
-              criarFuncionario(input: $input) {
-                id
-                cpf
-                nome
-                salario_base
-                admissao
-                desligamento
-          }
-        }`,
+        query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+                    }
+                }`,
         variables: {
           input: {
-            admissao: "2026-01-05",
             cpf: `${cpf}`,
+            nome: "IARA STEVANI",
+            salario_base: 8500.85,
+            admissao: "2026-01-05",
             desligamento: "",
-            nome: "paulo carvalho",
-            salario_base: 8500,
           },
         },
       });
+
     expect(resposta.status).to.equal(200);
     expect(resposta.body.data.criarFuncionario).to.have.property("id");
+  });
+
+  it("deve criar um funcinário quando preencho todos os campos de forma válida", async () => {
+    let cpf = Date.now();
+
+    const resposta = await request("http://localhost:4000")
+      .post("/graphql")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+                    }
+                }`,
+        variables: {
+          input: {
+            cpf: `${cpf}`,
+            nome: "JOAOZINHO",
+            salario_base: 8500.85,
+            admissao: "2026-01-05",
+            desligamento: "2026-10-20",
+          },
+        },
+      });
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.body.data.criarFuncionario).to.have.property("id");
+  });
+
+  it("não deve criar um funcinário quando não informo o salário base", async () => {
+    let cpf = Date.now();
+
+    const resposta = await request("http://localhost:4000")
+      .post("/graphql")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+                    }
+                }`,
+        variables: {
+          input: {
+            cpf: `${cpf}`,
+            nome: "JOAOZINHO",
+            admissao: "2026-01-05",
+            desligamento: "2026-10-20",
+          },
+        },
+      });
+
+    expect(resposta.status).to.equal(400);
+    expect(resposta.body.errors[0]).to.have.property(
+      "message",
+      `Variable \"$input\" got invalid value { cpf: \"${cpf}\", nome: \"JOAOZINHO\", admissao: \"2026-01-05\", desligamento: \"2026-10-20\" }; Field \"salario_base\" of required type \"Float!\" was not provided.`
+    );
+  });
+
+  it("não deve criar um funcinário quando a data de desligamento é inferior à data de admissão", async () => {
+    let cpf = Date.now();
+
+    const resposta = await request("http://localhost:4000")
+      .post("/graphql")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+                    }
+                }`,
+        variables: {
+          input: {
+            cpf: `${cpf}`,
+            nome: "JOAOZINHO",
+            salario_base: 8500.85,
+            admissao: "2026-08-05",
+            desligamento: "2026-08-04",
+          },
+        },
+      });
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.body.errors[0]).to.have.property(
+      "message",
+      `Desligamento não pode ser anterior à admissão.`
+    );
+  });
+
+  it("deve criar um funcinário quando preencho todos os campos de forma válida", async () => {
+    let cpf = Date.now();
+
+    const resposta = await request("http://localhost:4000")
+      .post("/graphql")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+                    }
+                }`,
+        variables: {
+          input: {
+            cpf: `${cpf}`,
+            nome: "MARIA",
+            salario_base: 5500.85,
+            admissao: "2026-01-10",
+          },
+        },
+      });
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.body.data.criarFuncionario).to.have.property("id");
+
+    const resposta2 = await request("http://localhost:4000")
+      .post("/graphql")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+                    }
+                }`,
+        variables: {
+          input: {
+            cpf: `${cpf}`,
+            nome: "JOSÉ",
+            salario_base: 3800.2,
+            admissao: "2026-06-01",
+          },
+        },
+      });
+
+    expect(resposta2.status).to.equal(200);
+    expect(resposta2.body.errors[0]).to.have.property(
+      "message",
+      "Já existe funcionário com este CPF."
+    );
   });
 });
--- test/specs/mutations/login.test.js
+++ test/specs/mutations/login.test.js
@@ -1,21 +1,12 @@
 const request = require("supertest");
 const { expect } = require("chai");
+const { login } = require("../../helpers/login.js");
+const loginData = require("../../fixtures/login.json");
 
-describe("mutation - login", () => {
-  it("deve realizar login com sucesso quando nao informo credenciais validas", async () => {
-    const resposta = await request("http://localhost:4000")
-      .post("/graphql")
-      .send({
-        query: `mutation Mutation($email: String!, $senha: String!) {
-        login(email: $email, senha: $senha) {
-              token 
-        }
-      }`,
-        variables: {
-          email: "admin@admin.com",
-          senha: "123456",
-        },
-      });
+describe("Mutation - Login", () => {
+  it("deve realizar login com sucesso quando informo credenciais válidas", async () => {
+    const resposta = await login(loginData.admin);
+
     expect(resposta.status).to.equal(200);
     expect(resposta.body.data.login).to.have.property("token");
     expect(resposta.body.data.login.token).to.not.be.empty;
@@ -24,20 +15,11 @@ describe("mutation - login", () => {
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
     );
   });
-  it("nao deve realizar login quando informo credenciais invalidas", async () => {
-    const resposta = await request("http://localhost:4000")
-      .post("/graphql")
-      .send({
-        query: `mutation Mutation($email: String!, $senha: String!) {
-        login(email: $email, senha: $senha) {
-              token 
-        }
-      }`,
-        variables: {
-          email: "admin@admin.com",
-          senha: "1234567",
-        },
-      });
+
+  it("não deve realizar login quando informo credenciais inválidas", async () => {
+    const usuario = { ...loginData.admin, senha: "1234567" };
+    const resposta = await login(usuario);
+
     expect(resposta.status).to.equal(200);
     expect(resposta.body.errors[0]).to.have.property(
       "message",
@@ -45,23 +27,35 @@ describe("mutation - login", () => {
     );
   });
 
-  it("nao deve realizar login quando informo email", async () => {
-    const resposta = await request("http://localhost:4000")
-      .post("/graphql")
-      .send({
-        query: `mutation Mutation($email: String!, $senha: String!) {
-        login(email: $email, senha: $senha) {
-              token 
-        }
-      }`,
-        variables: {
-          senha: "123456",
-        },
-      });
+  it("não deve realizar login quando não informo o email", async () => {
+    const { email, ...usuario } = loginData.admin;
+    const resposta = await login(usuario);
+
     expect(resposta.status).to.equal(400);
     expect(resposta.body.errors[0]).to.have.property(
       "message",
       'Variable "$email" of required type "String!" was not provided.'
+    );
+  });
+
+  it("não deve realizar login quando não informo a senha", async () => {
+    const { senha, ...usuario } = loginData.admin;
+    const resposta = await login(usuario);
+
+    expect(resposta.status).to.equal(400);
+    expect(resposta.body.errors[0]).to.have.property(
+      "message",
+      'Variable "$senha" of required type "String!" was not provided.'
+    );
+  });
+
+  it("não deve realizar login quando informo credenciais de um usuário inativo", async () => {
+    const resposta = await login(loginData.pgats);
+
+    expect(resposta.status).to.equal(200);
+    expect(resposta.body.errors[0]).to.have.property(
+      "message",
+      "Credenciais inválidas ou usuário inativo."
     );
   });
 });
